455fa739d6decfd032582d43557a214bdd386bd7
Eduardo <duleiteee@gmail.com>
2022-10-12 21:56:10 -0300

    ajust config

--- src/server.ts
+++ src/server.ts
@@ -2,19 +2,32 @@ import express, { Request, Response } from 'express'
 import moongose from 'mongoose'
 import router from './routes'
 import axios from "axios";
-import GamesController from './controllers/GamesController';
-
-const nodeSchedule = require('node-schedule')
-
-const data = new Date(2022, 9, 12, 17, 30);
 
 //const job = nodeSchedule.scheduleJob('*/15 23 * * 1-6', () => {
-    nodeSchedule.scheduleJob('*/15 21 * * 1-6', async () => {
+
+const app = express()
+app.use(express.json())
+app.use(router)
+
+moongose.connect('mongodb+srv://user:admin10@cluster0.qadnzlu.mongodb.net/?retryWrites=true&w=majority')
+.then((data) => {
+    console.log('MongoDB Connection Succeeded')
+})
+.catch((err) => {
+    console.log('Error DB Connection ',err.message)
+})
+
+app.get('/',(req:Request,res:Response) => {
+    return res.send('Hellow World!')
+})
+
+app.listen(3333)
+
+const nodeSchedule = require('node-schedule')
+nodeSchedule.scheduleJob('*/15 22 * * 1-6', async () => {
     console.log(Date())
 
     const latestGame = await axios.get("https://loteriascaixa-api.herokuapp.com/api/quina/latest")
-
-    const allGames = await axios.get("http://localhost:3333/games")
 
     var i = 1
 
@@ -41,21 +54,3 @@ const data = new Date(2022, 9, 12, 17, 30);
         i++
     }
 })
-
-const app = express()
-app.use(express.json())
-app.use(router)
-
-moongose.connect('mongodb+srv://user:admin10@cluster0.qadnzlu.mongodb.net/?retryWrites=true&w=majority')
-.then((data) => {
-    console.log('MongoDB Connection Succeeded')
-})
-.catch((err) => {
-    console.log('Error DB Connection ',err.message)
-})
-
-app.get('/',(req:Request,res:Response) => {
-    return res.send('Hellow World!')
-})
-
-app.listen(3333)
